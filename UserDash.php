6df432857dcf238adb8a6828fe911148fb4f81a3
jcservanocortes <45411917+jcservanocortes@users.noreply.github.com>
2018-11-28 11:37:48 +0800

    Add files via upload

---
+++ UserDash.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+       
+        <link rel="stylesheet" type="text/css" href="lcwcss/user.css">
+         <title>UserDashboard</title>
+            <body>
+                    <div class="sidebar_menu">
+                        <center> 
+                            <div class= "picbox">
+                            <img src="tao2.png" class="tao1">
+                            </div><br>
+                              <h1 class="box">USER</h1><br>
+                            <li><a href="UserDash.php" id="ac" >MY FILES</a></li>
+                            <li><a href="Userfilem.php" id="ho">DATA NAVIGATION</a></li>
+                            <li ><a href="loginform.php" id="loi">LOGOUT</a></li>
+                        
+                        </center>
+                    </div>
+                  <div class="box1">
+                             <p>Low-Cost Wireless Local Cloud</p>
+                        </div>
+                <div class="box2">
+                         <div class="boxx">
+                             <p>MY FILES</p>
+                             </div>
+                    <form>
+                    <div class="boc">
+                        <div class="search">
+                    <input width="50%" type="search" value=" " >
+            <input type="button" value="Search">
+                    </div>
+                        <div class="doc">
+                        <a href="documentfolder.php">
+                        <img src="lcwcss/fold.png" class="fol" >
+                            </a>
+                        <p>DOCUMENTS</p>
+                    </div>
+                    <div class="pic">
+                         <a href="picturefolder.php">
+                     <img src="lcwcss/fold.png" class="fol" href="Picture.php">
+                        </a>
+                        <p>PICTURE</p>
+                    </div>
+                <div class="mu">
+                     <a href="musicfolder.php">
+                    <img src="lcwcss/fold.png" class="fol"> 
+                    </a>
+                    <p>MUSIC</p>
+                    </div>
+                    </div>
+                    </form>
+                    </div>
+        </body>
+    </head>
+</html>
+               
